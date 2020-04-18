1ae9315f5f4447096c68dce85ca4e84cf7cf235b
petesamper <petesamper@gmail.com>
2020-04-18 01:11:13 -0400

    feat: feature: isEqual. Adds the isEqual function. All tests passing | Fix: Small bug fix in tests
    issue #4

--- src/tests/isEqual.test.js
+++ src/tests/isEqual.test.js
@@ -1,10 +1,10 @@
-const expect = require("chai").expect;
-const isEqual = require("../isEqual");
+const expect = require('chai').expect;
+const isEqual = require('../isEqual');
 
-describe("isEqual", () => {
-  xit("compares two objects and returns true if they have identical keys and values", () => {
-    const objOne = { name: { first: "John" } };
-    const objTwo = { name: { first: "John" } };
+describe('isEqual', () => {
+  it('compares two objects and returns true if they have identical keys and values', () => {
+    const objOne = { name: { first: 'John' } };
+    const objTwo = { name: { first: 'John' } };
     expect(isEqual(objOne, objTwo)).to.equal(true);
   });
   xit("compares two objects and returns false if they have non-identical keys and values", () => {
@@ -13,6 +13,7 @@ describe("isEqual", () => {
     expect(isEqual(objOne, objTwo)).to.equal(false);
   });
   xit("compares two arrays and returns true is they have identical values", () => {
+  it('compares two arrays and returns true is they have identical values', () => {
     const arrOne = [1, [2, [3]]];
     const arrTwo = [1, [2, [3]]];
     expect(isEqual(arrOne, arrTwo)).to.equal(true);
