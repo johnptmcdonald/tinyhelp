e4263f610d87d396fb6f2914bebed2b60f89990c
johnptmcdonald <johnptmcdonald@gmail.com>
2020-04-18 14:29:05 -0400

    test(index.js): adds camelCase to index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as isPrime } from "./isPrime.js";
 export { default as peek } from "./peek.js";
 export { default as sieveOfE } from "./sieveOfE.js";
 export { default as sort } from "./sort.js";
+export { default as camelCase } from "./camelCase.js";
--- src/tests/camelCase.test.js
+++ src/tests/camelCase.test.js
@@ -1,32 +1,32 @@
-const expect = require('chai').expect;
-const camelCase = require('../camelCase');
+const expect = require("chai").expect;
+const camelCase = require("../").camelCase;
 
-describe('camelCase', () => {
-	it('It returns an string if string is empty', () => {
-		const empty = '';
-		expect(camelCase(empty)).to.equal('');
-	});
+describe("camelCase", () => {
+  it("It returns an string if string is empty", () => {
+    const empty = "";
+    expect(camelCase(empty)).to.equal("");
+  });
 
-	it('returns one combined word', () => {
-		const oneWord = 'test';
-		const twoWords = 'This is working';
-		expect(camelCase(oneWord)).to.equal('test');
-		expect(camelCase(twoWords)).to.equal('thisIsWorking');
-	});
+  it("returns one combined word", () => {
+    const oneWord = "test";
+    const twoWords = "This is working";
+    expect(camelCase(oneWord)).to.equal("test");
+    expect(camelCase(twoWords)).to.equal("thisIsWorking");
+  });
 
-	it('returns all lowercase letters for the first word', () => {
-		const mixedCases = 'aBRa';
-		const moreMixedCases = 'KADabra';
-		expect(camelCase(mixedCases)).to.equal('abra');
-		expect(camelCase(moreMixedCases)).to.equal('kadabra');
-	});
+  it("returns all lowercase letters for the first word", () => {
+    const mixedCases = "aBRa";
+    const moreMixedCases = "KADabra";
+    expect(camelCase(mixedCases)).to.equal("abra");
+    expect(camelCase(moreMixedCases)).to.equal("kadabra");
+  });
 
-	it('returns camel case for sentences', () => {
-		const mixedSentence = 'ShE seLLS SeaSheLlS';
-		const spicyNoods = 'I like to eat Spicy NOODLES';
-    const randomWords = "cOMBinG THroUgh sOMe T e S T cases"
-		expect(camelCase(mixedSentence)).to.equal('sheSellsSeashells');
-		expect(camelCase(spicyNoods)).to.equal('iLikeToEatSpicyNoodles');
-		expect(camelCase(randomWords)).to.equal('combingThroughSomeTESTCases');
-	});
+  it("returns camel case for sentences", () => {
+    const mixedSentence = "ShE seLLS SeaSheLlS";
+    const spicyNoods = "I like to eat Spicy NOODLES";
+    const randomWords = "cOMBinG THroUgh sOMe T e S T cases";
+    expect(camelCase(mixedSentence)).to.equal("sheSellsSeashells");
+    expect(camelCase(spicyNoods)).to.equal("iLikeToEatSpicyNoodles");
+    expect(camelCase(randomWords)).to.equal("combingThroughSomeTESTCases");
+  });
 });
