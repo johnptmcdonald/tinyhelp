52dc4438af211f6d26b8c8edfa023012dd737d2a
Alexander Wang <alexander.wang22@gmail.com>
2020-04-18 08:34:24 -0400

    fix(isprime.js and isprime.test.js): more thorough prime checker and tests
    checks 1, 0, negative numbers correctly. tests for integer values as well.
    
    fix #5

--- src/isPrime.js
+++ src/isPrime.js
@@ -5,7 +5,9 @@
  */
 
 const isPrime = number => {
-  if (number === 1 || number === 2) {
+  if (!Number.isInteger(number)) return false
+  if (number <= 1) return false
+  if (number === 2) {
     return true;
   }
   if (number % 2 === 0) return false;
--- src/tests/isPrime.test.js
+++ src/tests/isPrime.test.js
@@ -8,4 +8,18 @@ describe("isPrime", () => {
     expect(isPrime(num)).to.be.true;
     expect(isPrime(num2)).to.be.false;
   });
+  it("returns false if a number is less than or equal to 1", () => {
+    const num = -7;
+    const num2 = 0;
+    expect(isPrime(num)).to.be.false;
+    expect(isPrime(num2)).to.be.false;
+  });
+  it("returns false if a number is not an integer", () => {
+    const num = 7.3;
+    const num2 = 31.5;
+    const num3 = 5.0
+    expect(isPrime(num)).to.be.false;
+    expect(isPrime(num2)).to.be.false;
+    expect(isPrime(num3)).to.be.true;
+  });
 });
