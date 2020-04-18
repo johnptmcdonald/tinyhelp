0889358a595e24132566ab5608c0df50899cf2bd
petesamper <petesamper@gmail.com>
2020-04-18 01:11:13 -0400

    feat: feature: isEqual. Adds the isEqual function. All tests passing | Fix: Small bug fix in tests
    issue #4

--- src/isEqual.js
+++ src/isEqual.js
@@ -1,1 +1,36 @@
-// in progress
+/**
+ * compares two objects or arrays
+ * @param {Array or Object} a - The Array or Object to compare
+ * @param {Array or Object} b - The Array or Object to compare
+ * @return {boolean}
+ */
+const isEqual = (a, b) => {
+  // Assumes that both params will be of the same data structure type.
+  // If arrays:
+  if (Array.isArray(a)) {
+    for (let i = 0; i <= a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    // If objects:
+  } else {
+    // Grab the keys:
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+
+    // if !same # of keys, return false:
+    if (aKeys.length !== bKeys.length) {
+      return false;
+    }
+
+    // Shallow check. If length of values is the same, return true:
+    if (Object.values(a).length === Object.values(b).length) {
+      return true;
+    }
+  }
+};
+
+module.exports = isEqual;
--- src/tests/isEqual.test.js
+++ src/tests/isEqual.test.js
@@ -1,15 +1,15 @@
-const expect = require("chai").expect;
-const isEqual = require("../isEqual");
+const expect = require('chai').expect;
+const isEqual = require('../isEqual');
 
-describe("isEqual", () => {
-  xit("compares two objects and returns true if they have identical keys and values", () => {
-    const objOne = { name: { first: "John" } };
-    const objTwo = { name: { first: "John" } };
+describe('isEqual', () => {
+  it('compares two objects and returns true if they have identical keys and values', () => {
+    const objOne = { name: { first: 'John' } };
+    const objTwo = { name: { first: 'John' } };
     expect(isEqual(objOne, objTwo)).to.equal(true);
   });
-  xit("compares two arrays and returns true is they have identical values", () => {
+  it('compares two arrays and returns true is they have identical values', () => {
     const arrOne = [1, [2, [3]]];
     const arrTwo = [1, [2, [3]]];
-    expect(isEqual(objOne, objTwo)).to.equal(true);
+    expect(isEqual(arrOne, arrTwo)).to.equal(true);
   });
 });
