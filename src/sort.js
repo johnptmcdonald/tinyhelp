dde319ca23be725b4bf05337d66f12c2d323d5b0
Erick <ecanals07@gmail.com>
2020-04-18 18:27:55 -0400

    Defined sort function. Still need to implement all the logic

--- src/sort.js
+++ src/sort.js
@@ -9,3 +9,13 @@
 // 0 denotes randomize the order
 // any other value will be ignored
 // returns sorted array without mutating original
+
+const sort = (array, order = 1) => {
+  if (array.length <= 1) return array;
+  let copy = [...array];
+
+  if (order === 1) {
+  } else if (order === -1) {
+  } else if (order === 0) {
+  }
+};
