81bf4d8f061129eedfea2f91d135df00fc8984d7
petesamper <petesamper@gmail.com>
2020-04-18 22:18:51 -0400

    figured it out

--- src/isEqual.js
+++ src/isEqual.js
@@ -4,6 +4,12 @@
  * @param {Array or Object} b - The Array or Object to compare
  * @return {boolean}
  */
+
+// Helper returns a value's internal object [[Class]];
+const getClass = (obj) => {
+  return Object.prototype.toString.call(obj);
+};
+
 const isEqual = (a, b) => {
   // Assumes that both params will be of the same data structure type.
   // If arrays:
@@ -17,6 +23,30 @@ const isEqual = (a, b) => {
     }
     // If objects:
   } else {
+    // If a and b reference the same value, return true:
+    if (a === b) return true;
+
+    // If a and b are !both Objects, return false:
+    if (typeof a != typeof b) return false;
+
+    // If type is number:
+    // TODO
+
+    // Get internal [[class]]:
+    const aClass = getClass(a);
+    const bClass = getClass(b);
+    // If classes are different, return false:
+    if (aClass != bClass) return false;
+
+    // If String, Number, or Boolean objects:
+    if (
+      aClass == '[object Boolean]' ||
+      aClass == '[object String]' ||
+      aClass == '[object Error]'
+    ) {
+      if (a.toString() != b.toString()) return false;
+    }
+
     // Grab the keys:
     const aKeys = Object.keys(a);
     const bKeys = Object.keys(b);
@@ -26,11 +56,21 @@ const isEqual = (a, b) => {
       return false;
     }
 
-    // Shallow check. If length of values is the same, return true:
-    if (Object.values(a).length === Object.values(b).length) {
-      return true;
+    // Check if they have the same keys:
+    if (
+      !aKeys.every((key) => {
+        return b.hasOwnProperty(key);
+      })
+    ) {
+      return false;
     }
+
+    // Check key values - recursion:
+    return aKeys.every((key) => {
+      return isEqual(a[key], b[key]);
+    });
   }
+  return false;
 };
 
 module.exports = isEqual;
