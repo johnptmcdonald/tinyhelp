23bd0f8432bdb1c78774f69e0c539916dc0fc6ed
petesamper <petesamper@gmail.com>
2020-04-20 21:38:38 -0400

    updated JSdoc and deleted junk

--- src/checkNested.js
+++ src/checkNested.js
@@ -1,14 +1,9 @@
 /**
  * A function to deeply compare objects
  * @param {Object} obj - Object to examine
- * @param {String} level - path of keys as seperated strings
+ * @param {String} key you are searching for
  * @return {Boolean}
  */
-// const checkNested = (obj, level, ...parameters) => {
-//   if (obj === undefined) return false;
-//   if (parameters.length == 0 && obj.hasOwnProperty(level)) return true;
-//   return checkNested(obj[level], ...parameters);
-// };
 
 const checkNested = (object, searchKey) => {
   // If bad input, return false:
